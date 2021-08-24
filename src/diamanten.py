71cb2648cdcf317052d3844ca2ef139927719d53
halfdane <halfdane@gmx.net>
2021-08-24 08:37:39 +0200

    add link to gettex - it's included in the volume calculation as well

--- src/diamanten.py
+++ src/diamanten.py
@@ -64,7 +64,8 @@ def create_message():
         "[%.4f](https://www.onvista.de/devisen/Eurokurs-Euro-Dollar-EUR-USD)) **volume: %s** ("
         "[TG](https://www.tradegate.de/orderbuch.php?isin=US36467W1099)+"
         "[LS-X](https://www.ls-x.de/de/aktie/gamestop-aktie)+"
-        "[LS-TC](https://www.ls-tc.de/de/aktie/gamestop-aktie))  " % (
+        "[LS-TC](https://www.ls-tc.de/de/aktie/gamestop-aktie)+"
+        "[GETTEX](https://www.gettex.de/suche/?tx_indexedsearch%%5Bsword%%5D=GS2C))  " % (
             tg['price'] * rate, tg['price'], rate, tg['volume'])
     ])
 
